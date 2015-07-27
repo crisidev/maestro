2fc336a5d74e1deb4e80624f3b188a0ff18e1bcc
Matteo Bigoi <bigo@crisidev.org>
2015-07-27 20:47:13 +0100

    App can issue commands on fleetctl and request cluster status without a config file loaded

--- cmd/maestro/main.go
+++ cmd/maestro/main.go
@@ -20,18 +20,23 @@ var (
 	app    = kingpin.New(APP_NAME, fmt.Sprintf("friendly generates and deploy systemd unit files on a CoreOS maestro cluster %s", APP_SITE))
 
 	// global
-	flagDebug          = app.Flag("debug", "enable debug mode").Short('d').Bool()
+	flagDebug          = app.Flag("debug", "enable debug mode").Short('D').Bool()
 	flagConfigFile     = app.Flag("config", "configuration file").Short('c').Default("maestro.json").String()
-	flagVolumesDir     = app.Flag("volumesdir", "directory on the coreos host for shared volumes").Short('V').Default("/var/maestro").String()
+	flagVolumesDir     = app.Flag("volumesdir", "directory on the coreos host for shared volumes").Short('V').Default("/share/maestro").String()
 	flagMaestroDir     = app.Flag("maestrodir", "directory on the local host for configs and temporary files (default to $USER/.maestro)").Short('m').String()
 	flagDomain         = app.Flag("domain", "domain used to deal with etcd, skydns, spartito and violino").Short('D').Default("maestro.io").String()
-	flagFleetEndpoints = app.Flag("etcd", "etcd / fleet endpoints to connect").Short('e').Default("http://172.17.8.101:2379,http://172.17.8.102:2379,http://172.17.8.103:2379").String()
+	flagFleetEndpoints = app.Flag("etcd", "etcd / fleet endpoints to connect").Short('e').String()
 	flagFleetOptions   = app.Flag("fleetopts", "fleetctl options").Short('f').Strings()
+	flagFleetAddress   = app.Flag("fleetaddr", "fleetctl tunnel address and port").Default("172.17.8.101").Short('f').String()
 
 	// cluster
 	flagCoreStatus = app.Command("corestatus", "report coreos cluster status")
 	flagExec       = app.Command("exec", "exec an arbitrary command through fleet, returning output as stdout and exit code")
 	flagExecArgs   = flagExec.Arg("args", "args to fleetclt command").Required().Strings()
+	flagEtcd       = app.Command("etcd", "get maestro related list of keys from etcd")
+	flagEtcdKey    = flagEtcd.Arg("name", "get one key").String()
+	flagEtcdSkydns = flagEtcd.Flag("skydns", "include skydns in the list of etcd keys").Short('d').Bool()
+	flagEtcdAll    = flagEtcd.Flag("all", "get the list of all etcd keys").Short('a').Bool()
 
 	// app
 	flagRun         = app.Command("run", "run current app on coreos (this will build unit files, submit and run them)")
@@ -70,20 +75,30 @@ func main() {
 		os.Exit(1)
 	}
 	// initialize maestro
-	maestro.Init(*flagMaestroDir, *flagDomain,
+	maestro.Init(*flagMaestroDir, *flagDomain, *flagFleetAddress,
 		*flagVolumesDir, *flagFleetEndpoints, *flagFleetOptions, *flagDebug)
 
-	if args == "corestatus" {
-		os.Exit(maestro.MaestroCoreStatus())
-	}
-	if args == "exec" {
+	exitCode = -1
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case flagCoreStatus.FullCommand():
+		exitCode = maestro.MaestroCoreStatus()
+	case flagExec.FullCommand():
 		go maestro.FleetExec(*flagExecArgs, fleetOutput, fleetExitCode)
-		os.Exit(maestro.FleetProcessOutput(fleetOutput, fleetExitCode))
+		exitCode = maestro.FleetProcessOutput(fleetOutput, fleetExitCode)
+	case flagEtcd.FullCommand():
+		exitCode = maestro.EtcdPullKeys(*flagEtcdSkydns, *flagEtcdAll, *flagEtcdKey)
 	}
+
+	if exitCode != -1 {
+		fmt.Printf("global exit code: %d\n", exitCode)
+		os.Exit(exitCode)
+	}
+
+	// build maestro config
 	config = maestro.BuildMaestroConfig(*flagConfigFile)
 
-	// command switch
 	exitCode = 0
+	// command switch
 	switch kingpin.MustParse(args, err) {
 
 	// info
